7ff3d21ccc8ae680b4e5b39e92827ad72cf8483b
hchen2020 <holdenychen@gmail.com>
2022-01-04 14:09:58 -0700

    Removed temporary database and switched to openFDA API

--- main.py
+++ main.py
@@ -1,62 +1,41 @@
 # Holden Chen
-# 09/18/2021
+# Updated 01/04/2022
 # RxBuddyApp - A Flask app that shows the user the common side effects for a given medication.
 
-from sqlite3.dbapi2 import connect
-from flask import Flask, render_template, request, g
-from datetime import datetime
-import sqlite3
-
-from flask.templating import render_template_string
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import requests
+import json
+import os
 
 
 app = Flask(__name__)
 
+load_dotenv()  # take environmental variable from .env.
 
-def connect_db():
-    medications_db = sqlite3.connect('medications.db', check_same_thread=False)
-    medications_db.row_factory = sqlite3.Row
-    return medications_db
-
-def get_db():
-    if not hasattr(g, 'sqlite3'):
-        g.sqlite3_db = connect_db()
-    return g.sqlite3_db
-
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite3_db.close()
-
-
-def get_side_effects(medication):
-    """Returns the common side effects for a given medication."""
-    try:
-        user_med = medication.lower()
-        db = get_db()
-
-        query = """SELECT * FROM medications WHERE drug_name = ?"""
-        arg = (user_med,)
-        cursor = db.execute(query, arg)
-        results = cursor.fetchone()
-        cursor.close()
-
-        return results[2]
-    except ValueError:
-        return "ERROR"
+API_KEY = os.getenv("API_KEY")
 
 # Index page with the UI
 @app.route("/", methods=["POST", "GET"])
 def index():
-    medications = request.form.getlist('drug[]')
-    results = []
-    medication = None
-    for medication in medications:
-        if medication:
-            results.append(get_side_effects(medication))
-        else:
-            results.append("NOT FOUND")
-    return render_template("index.html", meds_side_effects=zip(medications, results), meds=medications)
+    if request.method == "POST":
+        drug_1 = request.form["drug_1"]
+        drug_2 = request.form["drug_2"]
+        drug_3 = request.form["drug_3"]
+        drug_4 = request.form["drug_4"]
+        drug_dict = {drug_1: None, drug_2: None, drug_3: None, drug_4: None}
+
+        for drug_name in drug_dict:
+            if drug_name == "":
+                continue
+            url = f"https://api.fda.gov/drug/label.json?api_key={API_KEY}&search=adverse_reactions:{drug_name}"
+            req = requests.get(url)
+            drug_data = json.loads(req.content)
+            drug_dict[drug_name] = drug_data["results"][0]["adverse_reactions"][0]
+
+        return render_template("index.html", drug_dict=drug_dict)
+
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=False)
